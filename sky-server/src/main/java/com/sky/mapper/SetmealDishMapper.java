ce36eda37069bff68c864f212e9f08e391cec581
bluehyssopu <1767807919@qq.com>
2023-12-21 14:48:01 +0800

    实现套餐批量删除的接口

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -21,4 +22,12 @@ public interface SetmealDishMapper {
      * @param setmealDishes
      */
     void insertBatch(List<SetmealDish> setmealDishes);
+
+    /**
+     * 根据套餐id 批量删除菜品与套餐的关联关系
+     * @param id
+     */
+    @Delete("delete from setmeal_dish where id = #{id}")
+    void deleteBySetmealId(Long setmealId);
+
 }
